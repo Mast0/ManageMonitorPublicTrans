1966548ac1c38eb31dcc333e68042dd7e019ba86
Mast0 <bogdanremuga@gmail.com>
2024-11-26 00:42:02 +0200

    #3 Create DbContext
    implement DbContext

---
+++ DAL/EF/MMPTContext.cs
@@ -0,0 +1,30 @@
+﻿using Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+	public sealed class MMPTContext : DbContext
+	{
+		public DbSet<Coordinates> Coordinates { get; set; }
+
+		public DbSet<Driver> Drivers { get; set; }
+
+		public DbSet<Route> Routes { get; set; }
+
+		public DbSet<RoutePoint> RoutePoints { get; set; }
+
+		public DbSet<Schedule> Schedules { get; set; }
+
+		public DbSet<Vechicle> Vechicles { get; set; }
+
+        public MMPTContext(DbContextOptions<MMPTContext> opt)
+			: base(opt)
+        {
+        }
+    }
+}
