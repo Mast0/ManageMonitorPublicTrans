76bb74790985e66563558c745d6b2f3e30787bf2
Mast0 <bogdanremuga@gmail.com>
2024-11-26 02:46:59 +0200

    #7 Create UnitOfWork
    Create and implement UnitOfWork

---
+++ DAL/EF/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Interfaces;
+using DAL.Repositories;
+using DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+	public sealed class UnitOfWork : IUnitOfWork
+	{
+		private readonly MMPTContext _context;
+
+        public UnitOfWork(MMPTContext context)
+        {
+            _context = context;
+        }
+
+        public IDriverRepository DriverRepository => new DriverRepository(_context);
+
+		public IRouteRepository RouteRepository => new RouteRepository(_context);
+
+		public IScheduleRepository ScheduleRepository => new ScheduleRepository(_context);
+
+		public IVechicleRepository VechicleRepository => new VechicleRepository(_context);
+
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+	}
+}
---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+	public interface IUnitOfWork
+	{
+		IDriverRepository DriverRepository { get; }
+
+		IRouteRepository RouteRepository { get; }
+
+		IScheduleRepository ScheduleRepository { get; }
+
+		IVechicleRepository VechicleRepository { get; }
+
+		Task SaveAsync();
+	}
+}
